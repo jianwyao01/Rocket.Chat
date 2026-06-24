828bb6bc0cecec1ea6ad0554942010ef0c715856
Aleksander Nicacio da Silva <aleksander.silva@rocket.chat>
2026-06-24 18:27:34 -0300

    refactor: improve component reusability

--- packages/ui-voip/src/views/MediaCallRoomSection/MediaCallRoomSection.tsx
+++ packages/ui-voip/src/views/MediaCallRoomSection/MediaCallRoomSection.tsx
@@ -15,6 +15,7 @@ import {
 import { useMediaCallInstance } from '../../context/MediaCallInstanceContext';
 import { useMediaCallView } from '../../context/MediaCallViewContext';
 import useRegisterView from '../../context/useRegisterView';
+import AppActions from '../../experimental/AppActionButtons/components/AppActions';
 import { useVisibleAppActions } from '../../experimental/AppActionButtons/hooks/useVisibleAppActions';
 import MediaCallCardList from '../MediaCallCardList';
 import PopoutDockPrompt from '../PopoutDockPrompt';
@@ -70,7 +71,9 @@ const MediaCallRoomSection = ({ showChat, onToggleChat, user, containerHeight }:
 
 	useRegisterView('room');
 
-	const visibleActions = useVisibleAppActions();
+	const appActions = useVisibleAppActions();
+
+	const showHeaderActions = appActions.length > 0;
 
 	if (!peerInfo || 'number' in peerInfo) {
 		return null;
@@ -88,7 +91,7 @@ const MediaCallRoomSection = ({ showChat, onToggleChat, user, containerHeight }:
 			aria-label={t('Voice_call')}
 			{...getSplitStyles(showChat)}
 		>
-			{visibleActions.length > 0 && <ActionStrip leftSlot={visibleActions} />}
+			{showHeaderActions && <ActionStrip leftSlot={<AppActions actions={appActions} />} />}
 			{isPopout ? <PopoutDockPrompt onClosePopout={onClosePopout} /> : <MediaCallCardList user={user} shouldWrapCards={shouldWrapCards} />}
 			<ActionStrip
 				leftSlot={
--- packages/ui-voip/src/views/MediaCallWidget/OngoingCall.tsx
+++ packages/ui-voip/src/views/MediaCallWidget/OngoingCall.tsx
@@ -19,6 +19,7 @@ import {
 } from '../../components';
 import { useMediaCallView } from '../../context/MediaCallViewContext';
 import AppActions from '../../experimental/AppActionButtons/components/AppActions';
+import { useVisibleAppActions } from '../../experimental/AppActionButtons/hooks/useVisibleAppActions';
 
 const OngoingCall = () => {
 	const { t } = useTranslation();
@@ -37,6 +38,8 @@ const OngoingCall = () => {
 
 	const transferDisabled = !supportedFeatures.includes('transfer');
 	const holdDisabled = !supportedFeatures.includes('hold');
+
+	const appActions = useVisibleAppActions();
 
 	// TODO: Figure out how to ensure this always exist before rendering the component
 	if (!peerInfo) {
@@ -73,7 +76,8 @@ const OngoingCall = () => {
 						<Divider width='100%' />
 					</Box>
 				) : null}
-				<AppActions />
+				<AppActions actions={appActions} vertical />
+				{appActions.length > 0 && <Divider />}
 				<ButtonGroup large>
 					<ActionButton
 						disabled={connecting || reconnecting}
